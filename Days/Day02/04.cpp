8df566f32c48d9851bc9caf0343790f93dab3ac2
wupsi <codehook25@gmail.com>
2022-06-21 04:38:45 +0500

    editing day02

--- Days/Day02/04.cpp
+++ Days/Day02/04.cpp
@@ -2,38 +2,38 @@
 
 struct stack{
 
-    private:
-        int pos;
-        int arr[100000];
+private:
+    int pos;
+    int arr[100000];
 
-    public:
-        stack(){
-            pos = 0;
-        }
+public:
+    stack(){
+        pos = 0;
+    }
 
-        void push(int x){
-            arr[pos++] = x;
-        }
+    void push(int x){
+        arr[pos++] = x;
+    }
 
-        void pop(){
-            if(!empty())
-                pos--;
-            else 
-                cout << "ERROR\n";
-        }
+    void pop(){
+        if(!empty())
+            pos--;
+        else 
+            cout << "ERROR\n";
+    }
 
-        int top(){
-            if(!empty())
-                return arr[pos - 1];
-            else
-                return -9999999;
-        }
+    int top(){
+        if(!empty())
+            return arr[pos - 1];
+        else
+            return -9999999;
+    }
 
-        bool empty(){
-            return pos == 0;
-        }
+    bool empty(){
+        return pos == 0;
+    }
 
-        int size(){
-            return pos;
-        }
+    int size(){
+        return pos;
+    }
 };
